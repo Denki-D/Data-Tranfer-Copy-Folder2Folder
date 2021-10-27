eda1ba4fc975bb64544b18a8789132c7ff2bec7c
Denis Gaščić <denisgascic9@gmail.com>
2021-10-27 13:28:22 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+public class Class1
+{
+    static void Main()
+    {
+        string ulaz = @"d:\ulaz";
+        string izlaz = @"d:\izlaz\";
+
+        string[] mapa = Directory.GetFiles(ulaz);
+
+        foreach (var datoteka in mapa)
+        {
+            Console.WriteLine(datoteka.Remove(0,8));
+            string imeDat = datoteka.Substring(ulaz.Length + 1);
+            File.Copy(Path.Combine(ulaz, imeDat), Path.Combine(izlaz, imeDat), true);
+            Console.WriteLine(imeDat + " kopirano");
+        }
+
+        Console.WriteLine("\n Kraj kopiranja!");
+        Console.ReadLine();
+
+    }
+}
